e95775702c948fbe7ce626f15c0209cbd79ea30d
HoneyBad3er <maram1r0v@yandex.ru>
2020-05-14 19:38:36 +0300

    Adding Databases

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 import os
 from flask import Flask
 
+from .database import db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(os.environ['RUN_TYPE'])
+
+    db.init_app(app)
+    with app.test_request_context():
+        db.create_all()
+
     from app.routes import custom_me
     app.register_blueprint(custom_me)
 
--- app/routes.py
+++ app/routes.py
@@ -6,20 +6,32 @@ from flask import (
     url_for
 )
 
+from app.forms import LoginForm
 
 custom_me = Blueprint('custom_me', __name__, template_folder='templates')
 
 
 @custom_me.route('/')
+@custom_me.route('/main', methods=['GET'])
 def main():
-    return "Main page"
+    # Mock user
+    user = {'username': 'Marat'}
+    return render_template('main.html', title='Home', user=user, login_link='/login')
 
 
 @custom_me.route('/login', methods=['GET', 'POST'])
 def login():
-    return "Login page"
+    login_form = LoginForm()
+    return render_template('login.html', title='Log In', form=login_form)
 
 
-@custom_me.route('/anutka', methods=['GET', 'POST'])
-def anutka():
-    return "ann page"
+@custom_me.route('/signin', methods=['GET', 'POST'])
+def signin():
+    return "Signin page"
+
+
+@custom_me.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
